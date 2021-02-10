5a01c7c440f3ccc215a39899c08d98d8b9bbd225
anyone06 <sekiguchi@framelunch.jp>
2021-02-10 14:37:01 +0900

    画像アップロード

---
+++ src/api/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' })); // postで渡せる容量を増やす
+app.use(bodyParser.json()); // これがないとexpressのpostパラメーターが空になる
+const { Storage } = require('@google-cloud/storage');
+const storage = new Storage({
+  projectId: 'pikachu-test',
+  keyFilename: './pikachu-test-8dc69a5db590.json',
+});
+const bucketName = 'ogpimage';
+const bucket = storage.bucket(bucketName);
+let file;
+
+app.get('/hello', (req, res) => {
+  res.json({ message: 'hello, api' });
+});
+
+app.post('/upload', (req, res) => {
+  const fileName = String(Math.round(Math.random() * 1000000)); // ファイル名。適当
+  file = bucket.file(fileName);
+  upload(req.body.imagebuffer).then(() => {
+    res.status(200).send(fileName);
+  });
+});
+
+async function upload(data) {
+  try {
+    const base64EncodedImageString = data.replace(
+      /^data:image\/\w+;base64,/,
+      ''
+    );
+    const imageBuffer = Buffer.from(base64EncodedImageString, 'base64');
+    await file.save(imageBuffer, {
+      metadata: { contentType: 'image/jpg' },
+    });
+  } catch (e) {
+    throw e;
+  }
+}
+
+module.exports = {
+  path: '/api',
+  handler: app,
+};
---
+++ src/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div>
+      <Share />
+      <div v-if="responseHello">
+        {{ responseHello.data }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, useAsync } from '@nuxtjs/composition-api';
+import axios from 'axios';
+
+export default defineComponent({
+  setup() {
+    const responseHello = useAsync(() =>
+      axios.get('http://localhost:3000/api/hello')
+    );
+    return {
+      responseHello,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+}
+</style>
